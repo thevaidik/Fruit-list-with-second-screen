ab11939161cdea6a6ed527e6acf0d6c5db065226
thevaidik <vaidik50000@gmail.com>
2024-03-08 23:20:03 +0530

    Initial Commit

---
+++ Fruit list with second screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fruit list with second screen
+//
+//  Created by Vaidik Dubey on 08/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fruit list with second screen/Fruit_list_with_second_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit_list_with_second_screenApp.swift
+//  Fruit list with second screen
+//
+//  Created by Vaidik Dubey on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Fruit_list_with_second_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
